91beead720a5900087094bc641b8134a7ae186ce
Husniye <husniyesakmak.91@gmail.com>
2020-12-18 21:36:28 +0100

    project modified

--- JAM101 - Week 04 - Find the suspect in the train/src/Suspect.java
+++ JAM101 - Week 04 - Find the suspect in the train/src/Suspect.java
@@ -2,26 +2,23 @@ import java.util.Scanner;
 
 public class Suspect {
     public static void main(String [] args){
-        Utility utility=new Utility();
-        int random=utility.randomNumber();
-        guessingNumberGame(random);
+        int random=Utility.randomNumber();
+        int trial=guessingNumberGame(random);
+        int score=Utility.totalScore(trial);
+        System.out.println("Total Score: "+score);
     }
-    public static void guessingNumberGame(int randomNumber)
+    public static int guessingNumberGame(int randomNumber)
     {
         // Scanner Class
         Scanner scanner = new Scanner(System.in);
 
-        //int number = 1 + (int)(10 * Math.random());
-
         // Given K trials
         int trial = 4;
-
         int index, guess;
-
         System.out.println("A number is chosen" + " between 1 to 10." + "Guess the number" + " within 4 trials.");
 
         // Interate over K Trials
-        for (index = 0; index < trial; index++) {
+        for (index = 0; index < 4; index++,trial--) {
 
             System.out.println("Guess the number:");
             guess = scanner.nextInt();
@@ -31,18 +28,18 @@ public class Suspect {
                 System.out.println("Congratulations!" + " You guessed the number.");
                 break;
             }
-            else if (randomNumber > guess && index != trial - 1) {
+            else if (randomNumber > guess ) {
                 System.out.println("The number is " + "greater than " + guess);
             }
-            else if (randomNumber < guess && index != trial - 1) {
+            else if (randomNumber < guess) {
                 System.out.println("The number is" + " less than " + guess);
             }
+            if (trial == 0) {
+                System.out.println("You have exhausted" + " K trials.");
+                System.out.println("The number was " + randomNumber);
+            }
         }
-
-        if (index == trial) {
-            System.out.println("You have exhausted" + " K trials.");
-
-            System.out.println("The number was " + randomNumber);
-        }
+        return trial;
     }
 }
+
--- JAM101 - Week 04 - Find the suspect in the train/src/Utility.java
+++ JAM101 - Week 04 - Find the suspect in the train/src/Utility.java
@@ -1,8 +1,14 @@
 import java.lang.Math;
 public class Utility {
-    public static int randomNumber(){
+    public static int randomNumber() {
         // Generate the numbers
-        int number = 1 + (int)(10 * Math.random());
+        int number = 1 + (int) (10 * Math.random());
+        System.out.println(number);
         return number;
+    }
+    //method calculating the total score
+    public static int totalScore(int pScore) {
+        int score=(25)*(pScore);
+        return score;
     }
 }
