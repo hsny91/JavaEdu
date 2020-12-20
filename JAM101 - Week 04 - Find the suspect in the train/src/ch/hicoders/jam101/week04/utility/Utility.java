5b2d125d58441b476f7a8daa267f0fc9ccd810dc
Husniye <husniyesakmak.91@gmail.com>
2020-12-20 20:56:34 +0100

    project modified

--- JAM101 - Week 04 - Find the suspect in the train/src/ch/hicoders/jam101/week04/utility/Utility.java
+++ JAM101 - Week 04 - Find the suspect in the train/src/ch/hicoders/jam101/week04/utility/Utility.java
@@ -3,11 +3,13 @@ package ch.hicoders.jam101.week04.utility;
 import java.lang.Math;
 
 public class Utility {
+
     // Generate the numbers
     public static int randomNumber() {
         int number = 1 + (int) (10 * Math.random());
         return number;
     }
+
     //method calculating the total score
     public static int totalScore(int trial) {
         int score=25*trial;
