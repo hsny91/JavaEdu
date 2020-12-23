85c57488cb00060119aa081b6e44f8f98add7c0e
Husniye <husniyesakmak.91@gmail.com>
2020-12-23 14:43:42 +0100

    project modified

--- JAM101 - Week 05 - String Operations/src/Operations.java
+++ JAM101 - Week 05 - String Operations/src/Operations.java
@@ -1,3 +1,5 @@
+import java.util.Random;
+
 public class Operations {
     public static void main(String[] args){
         convertToUpperCase("asik veysel");
@@ -6,7 +8,10 @@ public class Operations {
         findPrimeNumber();
         System.out.println("\n10x10 Matris: ");
         renderMatris();
+        System.out.println("Passwords: ");
+        generatePassword();
     }
+
     /**
      *  String olarak verilen bir isim ve soyismin sadece bas harflerini Uppercase'e cevirme.
      */
@@ -18,8 +23,10 @@ public class Operations {
         }
         System.out.println(capitalNameSurname);
         return capitalNameSurname;
-    }/**
-     * 1 den 100 kadar olan sayilari 10x10 seklinde yazdiran fonksiyon
+    }
+
+    /**
+     * 1 den 100 kadar olan sayilari 10x10 seklinde ve her sayinin arasinda virgul olacak sekilde yazdiran fonksiyon
      * Oncelille 1-100 arasindaki sayilari StringBuild'in(numberList) icinde atiyor.10 ve 10 nun katlarina altsatira
      * gececek sekilde baska bir stringBuilder'e(totalString) atiyor ve bunu ekrana yazdiriyor.
      */
@@ -35,6 +42,7 @@ public class Operations {
         }
         System.out.println(totalString);
     }
+
     /**
      * Asal sayilari bulan ve ekrana yazdiran method
      */
@@ -51,6 +59,32 @@ public class Operations {
             if (controlPrime) {
                 System.out.print(primeNumber+",");
             }
+        }
+    }
+
+    /**
+     *  Kucuk, buyuk, ozel karakterler ve sayilardan olusan 8 ile 16 (hane) uzunluklari arasinda guvenli 50 adet sifre olusturan fonksiyon
+     */
+    private static void generatePassword() {
+        String capitalCaseLetters = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+        String lowerCaseLetters = "abcdefghijklmnopqrstuvwxyz";
+        String specialCharacters = "!@#$";
+        String numbers = "1234567890";
+        String combinedChars = capitalCaseLetters + lowerCaseLetters + specialCharacters + numbers;
+
+        for (int index = 0; index < 50; index++) {
+            Random random = new Random();
+            int passwordLength = random.nextInt(8) + 8;
+            char[] password = new char[passwordLength];
+            password[0] = lowerCaseLetters.charAt(random.nextInt(lowerCaseLetters.length()));
+            password[1] = capitalCaseLetters.charAt(random.nextInt(capitalCaseLetters.length()));
+            password[2] = specialCharacters.charAt(random.nextInt(specialCharacters.length()));
+            password[3] = numbers.charAt(random.nextInt(numbers.length()));
+
+            for (int indexPass = 4; indexPass < passwordLength; indexPass++) {
+                password[indexPass] = combinedChars.charAt(random.nextInt(combinedChars.length()));
+            }
+            System.out.println(password);
         }
     }
     /**
