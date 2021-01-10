8971a85a30d00e823acaa565f241b7777daff6a7
Husniye <husniyesakmak.91@gmail.com>
2021-01-10 23:35:30 +0100

    project was created

--- JAM101 - Week 06 - Strings, Arrays, ArrayList and LocalDate/src/Main.java
+++ JAM101 - Week 06 - Strings, Arrays, ArrayList and LocalDate/src/Main.java
@@ -1,5 +1,78 @@
-public class Mains {
+import java.time.LocalDate;
+import java.util.ArrayList;
+public class Main {
+    private static ArrayList<String> bikePartList = new ArrayList<>();
+
     public static void main(String [] args){
-        System.out.println("denme");
+        addPartInList();
+        createResultList();
+    }
+
+    private static void addPartInList () {
+        bikePartList.add("tsla342");
+        bikePartList.add("vw0929");
+        bikePartList.add("tyt567");
+        bikePartList.add("bmw6126");
+        bikePartList.add("cabrio213");
+        bikePartList.add("alfaromeo3234");
+    }
+
+    /**
+     * Parca isimleri büyük harflere cevrilecen Method
+     * @return
+     */
+    private static String convertToUpperCase (String pPartName) {
+        return pPartName.toUpperCase();
+    }
+
+    /**
+     * Her parcanin basina ASLIAG_ ekleyen Method
+     * @param pPartName
+     * @return
+     */
+    private static String addName (String pPartName) {
+        return"ASLIAG_"+pPartName;
+    }
+
+    /**
+     * Parca isimleri tersine cevren Method
+     * @param pPartName
+     * @return
+     */
+    private static String reversePartName (String pPartName) {
+        return new StringBuilder(pPartName).reverse().toString();
+    }
+
+    /**
+     * Parca isimlerinden sayilar cikartan Method
+     * @param pPartName
+     * @return
+     */
+    private static String dropNumbers(String pPartName) {
+        pPartName= pPartName.replaceAll("[0-9]","");
+        return pPartName;
+    }
+
+    /**
+     * Her parcanin sonuna ise parcanin sisteme girildigi tarih ekleyen Method.
+     * @param pPartName
+     * @return
+     */
+    public static String addDate(String pPartName){
+        LocalDate date = LocalDate.now();
+        return pPartName+date.toString();
+    }
+
+    /**
+     * Butun metodlari cagiran ve ekrana yazdiran Method
+     */
+    private static void createResultList () {
+        bikePartList.stream()
+                .map(Main::convertToUpperCase)
+                .map(Main::reversePartName)
+                .map(Main::addName)
+                .map(Main::dropNumbers)
+                .map(Main::addDate)
+                .forEach(System.out::println);
     }
 }
