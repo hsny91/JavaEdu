ce8c1a12240127c9cf706d1f3a6163f3bd1cf549
Husniye <husniyesakmak.91@gmail.com>
2021-01-16 12:39:41 +0100

    project was created

--- JAM101 - Week 07 - Time Converter/src/DateTimeFormats.java
+++ JAM101 - Week 07 - Time Converter/src/DateTimeFormats.java
@@ -1,6 +1,15 @@
-public class Main {
-    public static void main(String [] args){
-       
-    }
-}
+import java.text.SimpleDateFormat;
+import java.util.Date;
+import java.util.Locale;
+
+public class DateTimeFormats {
+
+            public static void main(String [] args) throws Exception {
+                SimpleDateFormat displayFormat = new SimpleDateFormat("HH:mm");
+                SimpleDateFormat parseFormat = new SimpleDateFormat("hh:mm a", Locale.ENGLISH);
+                Date date = parseFormat.parse("10:30 PM");
+                System.out.println(parseFormat.format(date) + " = " + displayFormat.format(date));
+            }
+        }
+
 
