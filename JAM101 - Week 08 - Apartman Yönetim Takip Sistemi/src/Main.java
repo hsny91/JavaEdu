ad1f8b4e0204c9755c6b588671faa09360bd2d48
Husniye <husniyesakmak.91@gmail.com>
2021-01-23 09:28:02 +0100

    project was modified

--- JAM101 - Week 08 - Apartman Yönetim Takip Sistemi/src/Main.java
+++ JAM101 - Week 08 - Apartman Yönetim Takip Sistemi/src/Main.java
@@ -3,49 +3,11 @@ import java.util.ArrayList;
 import java.util.Random;
 
 public class Main {
-    private static ArrayList<Aidat> aidatList = new ArrayList<Aidat>();
-    private static final double totalAidatPrice=(6250);
     public static void main(String args[]) {
-        ArrayList<Aidat> totalAidatList=createAidat() ;
-        createDaire(totalAidatList);
-
+        Manager manager=new Manager();
+        manager.start();
     }
 
-    public static ArrayList<Aidat> createAidat() {
-        for (int i = 1; i <= 12; i++) {
-            Random random=new Random(); //random sınıfı
-            double randomNumber=random.nextDouble()*500;
-            Aidat aidat = new Aidat(i, randomNumber);
-            aidatList.add(aidat);
-        }
-        return aidatList;
-    }
-
-    public static void createDaire( ArrayList<Aidat> pAidatList) {
-        for (int i = 1; i <= 12; i++) {
-            Daire daire = new Daire(pAidatList, i);
-            double totalAidat=findTotalAidat(daire.getDaireList());
-            double financialStatement=findFinancialStatement(totalAidat);
-            //System.out.println(totalAidat);
-           // System.out.println(financialStatement);
-            showFinancialStatement(financialStatement,daire.getDaireNo());
-        }
-    }
-    public static double findTotalAidat( ArrayList<Aidat> pAidatList) {
-        double total=0;
-        for (Aidat aidat : pAidatList) {
-           total+=aidat.amountOfPaid;
-        }
-        return total;
-    }
-    public static double findFinancialStatement(double pTotalAidat){
-        double result=totalAidatPrice-pTotalAidat;
-        return result;
-    }
-    public static void showFinancialStatement(double result,int pNumberDaire){
-        System.out.println(pNumberDaire+". Daire Yil sonu bilancosu: "+result);
-
-    }
 }
 
 
--- JAM101 - Week 08 - Apartman Yönetim Takip Sistemi/src/Manager.java
+++ JAM101 - Week 08 - Apartman Yönetim Takip Sistemi/src/Manager.java
@@ -1,2 +1,50 @@
+import java.util.ArrayList;
+import java.util.Random;
+
 public class Manager {
+    private static final double totalAidatPrice=(3000);
+
+    public static ArrayList<Aidat> createAidat() {
+        ArrayList<Aidat> aidatList = new ArrayList<Aidat>();
+        for (int i = 1; i <= 12; i++) {
+            Random random=new Random(); //random sınıfı
+            double randomNumber=random.nextDouble()*200;
+            Aidat aidat = new Aidat(i, randomNumber);
+            aidatList.add(aidat);
+        }
+        return aidatList;
+    }
+
+    public static void createDaire() {
+        for (int i = 1; i <= 5; i++) {
+            ArrayList<Aidat> totalAidatLıst=createAidat();
+            Daire daire = new Daire(totalAidatLıst, i);
+            double totalAidat=findTotalAidat(daire.getDaireList());
+            double financialStatement=findFinancialStatement(totalAidat);
+            showFinancialStatement(financialStatement,daire.getDaireNo());
+            /*for (Aidat aidat : daire.getDaireList()) {
+                System.out.println(+aidat.amountOfPaid + " " + aidat.mounthNumber);
+            }*/
+        }
+    }
+    public static double findTotalAidat( ArrayList<Aidat> pAidatList) {
+        double total=0;
+        for (Aidat aidat : pAidatList) {
+            total+=aidat.amountOfPaid;
+        }
+        System.out.println(total);
+        return total;
+    }
+    public static double findFinancialStatement(double pTotalAidat){
+        double result=pTotalAidat-totalAidatPrice;
+        int intResult = (int) result;
+        return intResult;
+    }
+    public static void showFinancialStatement(double result,int pNumberDaire){
+        System.out.println(pNumberDaire+". Daire Yil sonu bilancosu: "+result);
+
+    }
+    public static void start(){
+        createDaire();
+    }
 }
