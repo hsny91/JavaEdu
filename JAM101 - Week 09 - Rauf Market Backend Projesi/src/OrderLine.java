dd657ba1bedf089c380aca8c646c0d03c8438d74
Husniye <husniyesakmak.91@gmail.com>
2021-01-30 11:01:40 +0100

    project was modified

--- JAM101 - Week 09 - Rauf Market Backend Projesi/src/OrderLine.java
+++ JAM101 - Week 09 - Rauf Market Backend Projesi/src/OrderLine.java
@@ -1,2 +1,13 @@
+import java.util.ArrayList;
+
 public class OrderLine {
+    private Product product;
+    private int productQuantit;
+    private double totalItemPrice;
+
+    public OrderLine(Product pProduct, int pPoductQuantit,double pTotalItemPrice){ ;
+        this.product=pProduct;
+        this.productQuantit=pPoductQuantit;
+        this.totalItemPrice=pTotalItemPrice;
+    }
 }
